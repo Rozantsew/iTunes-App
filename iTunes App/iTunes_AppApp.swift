ae2ae6307eb5b87a2bc605bde2c6b13ff1aed063
Michał <mykhailorozantsev@gmail.com>
2022-11-22 16:36:18 +0100

    Initial Commit

---
+++ iTunes App/iTunes_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTunes_AppApp.swift
+//  iTunes App
+//
+//  Created by Mykhailo Rozantsev on 22/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct iTunes_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
